e5470658a545c83d8cd6c90d4f7476ee834f4a41
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2023-01-13 00:13:38 +0700

    Finish app

--- app/src/main/java/com/nandaiqbalh/mypassword/di/ServiceLocator.kt
+++ app/src/main/java/com/nandaiqbalh/mypassword/di/ServiceLocator.kt
@@ -1,6 +1,10 @@
 package com.nandaiqbalh.mypassword.di
 
 import android.content.Context
+import com.nandaiqbalh.mypassword.data.local.database.AppDatabase
+import com.nandaiqbalh.mypassword.data.local.database.dao.PasswordDao
+import com.nandaiqbalh.mypassword.data.local.database.datasource.PasswordDataSource
+import com.nandaiqbalh.mypassword.data.local.database.datasource.PasswordDataSourceImpl
 import com.nandaiqbalh.mypassword.data.local.preference.UserPreference
 import com.nandaiqbalh.mypassword.data.local.preference.UserPreferenceDataSource
 import com.nandaiqbalh.mypassword.data.local.preference.UserPreferenceDataSourceImpl
@@ -9,15 +13,31 @@ import com.nandaiqbalh.mypassword.data.repository.LocalRepositoryImpl
 
 object ServiceLocator {
 
-	fun provideUserPreference(context: Context): UserPreference{
+	fun provideUserPreference(context: Context): UserPreference {
 		return UserPreference(context)
 	}
 
-	fun provideUserPreferenceDataSource(context: Context): UserPreferenceDataSource{
+	fun provideUserPreferenceDataSource(context: Context): UserPreferenceDataSource {
 		return UserPreferenceDataSourceImpl(provideUserPreference(context))
 	}
 
-	fun provideLocalRepository(context: Context):LocalRepository{
-		return LocalRepositoryImpl(provideUserPreferenceDataSource(context))
+	fun provideAppDatabase(appContext: Context): AppDatabase {
+		return AppDatabase.getInstance(appContext)
 	}
+
+	fun providePasswordDao(appContext: Context): PasswordDao {
+		return provideAppDatabase(appContext).passwordDao()
+	}
+
+	fun providePasswordDataSource(appContext: Context): PasswordDataSource {
+		return PasswordDataSourceImpl(providePasswordDao(appContext))
+	}
+
+	fun provideLocalRepository(context: Context): LocalRepository {
+		return LocalRepositoryImpl(
+			provideUserPreferenceDataSource(context),
+			providePasswordDataSource(context)
+		)
+	}
+
 }
--- app/src/main/java/com/nandaiqbalh/mypassword/presentation/ui/passwordlist/PasswordListActivity.kt
+++ app/src/main/java/com/nandaiqbalh/mypassword/presentation/ui/passwordlist/PasswordListActivity.kt
@@ -7,33 +7,142 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nandaiqbalh.mypassword.R
+import com.nandaiqbalh.mypassword.data.local.database.entity.PasswordEntity
 import com.nandaiqbalh.mypassword.databinding.ActivityPasswordListBinding
 import com.nandaiqbalh.mypassword.di.ServiceLocator
 import com.nandaiqbalh.mypassword.presentation.ui.createappkey.CreateAppKeyBottomSheet
 import com.nandaiqbalh.mypassword.presentation.ui.createappkey.OnAppKeyChangedListener
 import com.nandaiqbalh.mypassword.presentation.ui.enterappkey.EnterAppKeyBottomSheet
 import com.nandaiqbalh.mypassword.presentation.ui.enterappkey.OnAppKeyConfirmedListener
+import com.nandaiqbalh.mypassword.presentation.ui.passwordform.PasswordFormActivity
+import com.nandaiqbalh.mypassword.presentation.ui.passwordlist.adapter.PasswordItemClickListener
+import com.nandaiqbalh.mypassword.presentation.ui.passwordlist.adapter.PasswordListAdapter
+import com.nandaiqbalh.mypassword.utils.ClipboardUtils
 import com.nandaiqbalh.mypassword.utils.viewModelFactory
+import com.nandaiqbalh.mypassword.wrapper.Resource
+import java.lang.reflect.Array.newInstance
 
 class PasswordListActivity : AppCompatActivity() {
 
 	private val binding: ActivityPasswordListBinding by lazy {
 		ActivityPasswordListBinding.inflate(layoutInflater)
 	}
-
 	private val viewModel: PasswordListViewModel by viewModelFactory {
 		PasswordListViewModel(ServiceLocator.provideLocalRepository(this))
+	}
+	private val adapter: PasswordListAdapter by lazy {
+		PasswordListAdapter(object : PasswordItemClickListener {
+			override fun onItemClicked(item: PasswordEntity) {
+				Toast.makeText(this@PasswordListActivity, "Item Clicked", Toast.LENGTH_SHORT).show()
+			}
+
+			override fun onItemLongClicked(item: PasswordEntity) {
+				ClipboardUtils.copyToClipboard(
+					this@PasswordListActivity,
+					item.username ?: item.email ?: "",
+					item.password.orEmpty()
+				)
+				Toast.makeText(
+					this@PasswordListActivity,
+					"Password Copied to Clipboard",
+					Toast.LENGTH_SHORT
+				).show()
+			}
+
+			override fun onDeleteMenuClicked(item: PasswordEntity) {
+				viewModel.deletePassword(item)
+			}
+
+			override fun onEditMenuClicked(item: PasswordEntity) {
+				startActivity(PasswordFormActivity.newInstance(this@PasswordListActivity, item.id))
+			}
+
+		})
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
+		initList()
+		setOnClickListeners()
+		observeData()
 	}
 
+	private fun setOnClickListeners() {
+		binding.fabAddData.setOnClickListener {
+			startActivity(PasswordFormActivity.newInstance(this))
+		}
+	}
+
+	private fun getData() {
+		viewModel.getPasswordList()
+	}
+
+	private fun initList() {
+		binding.rvPasswordList.apply {
+			layoutManager = LinearLayoutManager(this@PasswordListActivity)
+			adapter = this@PasswordListActivity.adapter
+		}
+	}
+
+	private fun observeData() {
+		viewModel.passwordListResult.observe(this) {
+			when (it) {
+				is Resource.Loading -> {
+					setLoadingState(true)
+					setErrorState(false)
+				}
+				is Resource.Success -> {
+					setLoadingState(false)
+					setErrorState(false)
+					bindDataToAdapter(it.payload)
+				}
+				is Resource.Error -> {
+					setLoadingState(false)
+					setErrorState(true, it.exception?.message.orEmpty())
+				}
+			}
+		}
+		viewModel.deleteResult.observe(this) {
+			when (it) {
+				is Resource.Loading -> {
+
+				}
+				is Resource.Success -> {
+					Toast.makeText(this, "Delete Password Success", Toast.LENGTH_SHORT).show()
+					getData()
+				}
+				is Resource.Error -> {
+					Toast.makeText(this, "Delete Password Failed", Toast.LENGTH_SHORT).show()
+				}
+			}
+		}
+	}
+
+	private fun bindDataToAdapter(data: List<PasswordEntity>?) {
+		if (data.isNullOrEmpty()) {
+			adapter.clearItems()
+			setErrorState(true, "Your password list is empty!")
+		} else {
+			adapter.setItems(data)
+		}
+	}
+
+	private fun setErrorState(isError: Boolean, errorMsg: String = "") {
+		binding.tvError.text = errorMsg
+		binding.tvError.isVisible = isError
+	}
+
+	private fun setLoadingState(isLoading: Boolean) {
+		binding.pbPasswordList.isVisible = isLoading
+		binding.rvPasswordList.isVisible = !isLoading
+	}
 
 	override fun onResume() {
 		super.onResume()
+		getData()
 		binding.root.isVisible = false
 		if (viewModel.checkIfAppKeyIsExist()) {
 			showDialogEnterAppKey(false) { isPasswordCorrect ->
